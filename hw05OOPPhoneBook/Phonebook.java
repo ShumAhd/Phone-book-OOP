201b10f982e5f2b9c29b70b7b8d9b84ed33d18ca
ShuMAhd <shuman@zykaika.ru>
2023-01-19 20:56:18 +0800

    Программа работает

---
+++ hw05OOPPhoneBook/Phonebook.java
@@ -0,0 +1,16 @@
+package hw05OOPPhoneBook;
+
+import java.util.ArrayList;
+import hw05OOPPhoneBook.Contact;
+
+public class Phonebook {
+  private static ArrayList<Contact> phonebook = new ArrayList<>();
+
+  public static ArrayList<Contact> getPhonebook(){
+    return phonebook;
+  }
+
+  public static void addContacts(Contact contact){
+    phonebook.add(contact);
+  }
+}
